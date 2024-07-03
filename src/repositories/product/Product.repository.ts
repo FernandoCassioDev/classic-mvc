fd6d02d56fa7ab9801785c16fa0d3a22b2b16259
Fernando Cassio <fernandocassiodev@gmail.com>
2024-07-03 19:29:39 -0300

    working in contracts

---
+++ src/repositories/product/Product.repository.ts
@@ -0,0 +1,9 @@
+import { Product } from "../../entities/Product";
+
+
+export interface ProductRepository {
+    save(product: Product):Promise<void>;
+    list(): Promise<Product[]>
+    update(product: Product): Promise<void>;
+    find(id: string): Promise<Product>;
+}
